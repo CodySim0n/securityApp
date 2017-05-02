2f1d7247589113a4fa798117af72252aa9dc8062
codysim0n <codyjsimon1@gmail.com>
2017-05-01 22:17:50 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var pythonShell = require('python-shell')
+var pcap = require('pcap')
+// pythonShell.run('sniffer.py', function(err){
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('finished');
+// })
+
+console.log(pcap);
